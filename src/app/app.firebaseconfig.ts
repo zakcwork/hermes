3c9e6375e44dc0aeb6f5756c628f8f375888ca6c
zakcwork <31586716+ZakGitHub@users.noreply.github.com>
2017-10-25 22:12:47 +0100

    fire bas auth integration

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,10 @@
+// export firebase configuration
+
+export var config = {
+    apiKey: "AIzaSyAhUKC2L0RhlpCbMjfrKyEBXaCabmByKbs",
+    authDomain: "hermes-18bc8.firebaseapp.com",
+    databaseURL: "https://hermes-18bc8.firebaseio.com",
+    projectId: "hermes-18bc8",
+    storageBucket: "hermes-18bc8.appspot.com",
+    messagingSenderId: "1027717396932"
+};
